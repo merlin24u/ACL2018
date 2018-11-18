9d966f1e80bd148344e9b9ca85a31dd294fb2648
Rafał Meisel <rafal.n.meisel@gmail.com>
2018-11-18 20:24:53 +0100

    Prepare des tests de Key class

---
+++ src/test/TestKey.java
@@ -0,0 +1,62 @@
+package test;
+
+import model.Key;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestKey {
+
+    String keyId;
+    String keyName;
+    Key key;
+
+    @Before
+    public void initialize(){
+        keyId = "keyId";
+        keyName = "keyName";
+        key = new Key(keyId, keyName);
+    }
+
+    @Test
+    public void testClone(){
+        assertEquals("Clone key n'est pas meme!", key, key.clone());
+    }
+
+    @Test
+    public void testEquals(){
+        Key newKey = new Key (keyId, keyName);
+
+        boolean result = key.equals(newKey);
+        assertEquals("Key n'est pas meme!", true, result);
+    }
+
+    @Test
+    public void testNotEqualsId(){
+        keyId = keyId + "New";
+        Key newKey = new Key (keyId, keyName);
+
+        boolean result = key.equals(newKey);
+        assertEquals("Key est meme!", false, result);
+    }
+
+    @Test
+    public void testNotEqualsName(){
+        keyName = keyName + "New";
+        Key newKey = new Key (keyId, keyName);
+
+        boolean result = key.equals(newKey);
+        assertEquals("Key est meme!", false, result);
+    }
+
+    @Test
+    public void testNotEqualsIdName(){
+        keyId = keyId + "New";
+        keyName = keyName + "New";
+        Key newKey = new Key (keyId, keyName);
+
+        boolean result = key.equals(newKey);
+        assertEquals("Key est meme!", false, result);
+    }
+}
